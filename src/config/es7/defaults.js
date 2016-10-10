3de9e51f5bf0f2a69bfdd8527d19792200166f8f
Andrew Monks <a@monks.co>
2016-10-10 13:13:53 -0500

    add destructuring, bump

--- src/config/es7/defaults.js
+++ src/config/es7/defaults.js
@@ -7,6 +7,7 @@ export default compose(
   addBabelPlugin('transform-decorators-legacy'),
   addBabelPlugin('transform-react-display-name'),
   addBabelPreset('react-optimize'),
+  addBabelPreset('transform-es2015-destructuring'),
   requireLoader('babel-loader')
 )
 
