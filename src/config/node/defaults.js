c29610242376a5bd85f85f176b7f729c3c8a3cae
Andrew Monks <a@monks.co>
2016-10-10 17:56:38 -0500

    adds externals to node builder
    On the server, we only want webpack to bundle our code, not node
    modules.
    
    We're using it mostly for babel transforms, not really for
    singe-entry-point-ing, and some server dependencies have binary
    components that can't safely be webpacked anyway.

--- src/config/node/defaults.js
+++ src/config/node/defaults.js
@@ -1,4 +1,22 @@
-import {set, lensProp} from 'ramda'
+import fs from 'fs'
+import {set, lensProp, compose, map} from 'ramda'
 
-export default set(lensProp('target'), 'node')
+const detupelize = arrOfTupels => {
+  let obj = {}
+  arrOfTupels.forEach(t => {
+    const [key, value] = t
+    obj[key] = value
+  })
+  return obj
+}
+
+const makeExterns = compose(
+  detupelize,
+  map(m => [m, `commonjs ${m}`])
+)
+
+export default compose(
+  set(lensProp('externals'), makeExterns(fs.readDirSync('node_modules'))),
+  set(lensProp('target'), 'node')
+)
 
