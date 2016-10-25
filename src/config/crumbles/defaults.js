e751dc962a91996ebea975e64f919360afdb9bfa
Andrew Monks <andrew@theuprisingcreative.com>
2016-10-25 18:05:54 -0500

    crumbles

--- src/config/crumbles/defaults.js
+++ src/config/crumbles/defaults.js
@@ -1,32 +1,21 @@
-import { evolve, omit, update, findIndex, compose } from 'ramda'
+import { evolve, omit, update, findIndex } from 'ramda'
 
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ doing crumble thing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-export default x => 'hello!'
-//
-//
-// export default compose(
-//   evolve({
-//     externals: omit(['.bin', 'crumbles', '.yarn-integrity']),
-//     module: {
-//       loaders: loaders => {
-//         console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ doing loader thing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-//         const babelLoaderIndex = findIndex(l => l.loader === 'babel-loader', loaders)
-//         const babelLoader = loaders[babelLoaderIndex]
-//         console.log({babelLoader, babelLoaderIndex, loaders})
-//         return update(
-//           babelLoaderIndex,
-//           {
-//             ...babelLoader,
-//             exclude: /node_modules\/(?!crumbles)|webpack\/hot/
-//           },
-//           loaders
-//         )
-//       }
-//     }
-//   }),
-//   x => {
-//     console.log('HELLO!@!!!!!!!!!!!!!!!!!!!!!!')
-//     return x
-//   }
-// )
+export default evolve({
+  externals: omit(['.bin', 'crumbles', '.yarn-integrity']),
+  module: {
+    loaders: loaders => {
+      const babelLoaderIndex = findIndex(l => l.loader === 'babel-loader', loaders)
+      const babelLoader = loaders[babelLoaderIndex]
+      console.log({babelLoader, babelLoaderIndex, loaders})
+      return update(
+        babelLoaderIndex,
+        {
+          ...babelLoader,
+          exclude: /node_modules\/(?!crumbles)|webpack\/hot/
+        },
+        loaders
+      )
+    }
+  }
+})
 
