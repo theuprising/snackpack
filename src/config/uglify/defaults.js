4818afe36dfe22620f4a6b50cea3804980770564
Andrew Monks <andrew@theuprisingcreative.com>
2016-11-14 10:06:34 -0600

    bump version

--- src/config/uglify/defaults.js
+++ src/config/uglify/defaults.js
@@ -1,7 +1,25 @@
-export default {
-  optimize: {
-    minimize: true,
-    dedupe: true
-  }
-}
+import { LoaderOptionsPlugin, optimize } from 'webpack'
+import { compose, evolve, append } from 'ramda'
 
+export default compose(
+  evolve({
+    plugins: append(new LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }))
+  }),
+  evolve({
+    plugins: append(new optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    }))
+  }),
+  evolve({
+    plugins: append(new optimize.DedupePlugin())
+  })
+)
