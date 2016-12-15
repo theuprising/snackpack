8d1e66ef99ec7db70caed69b2b424e9dadb704e0
Andrew Monks <a@monks.co>
2016-12-15 14:09:59 -0600

    fix babel support
    no more implicit use of the polyfill. If you want a polyfill, gotta add
    it yourself.

--- src/babel.js
+++ src/babel.js
@@ -8,19 +8,14 @@ import { evolve, append, prepend, map } from 'ramda'
  * @returns {WebpackConfig} config
  * @sig Config -> Config
  * @desc
- * adds babel and some babel plugins
+ * adds the babel loader for `/\.jsx?$/` files
  */
 export default evolve({
-  entry: map(prepend('babel-polyfill')),
   module: {
     loaders: append({
       test: /\.jsx?$/,
       exclude: /(node_modules|webpack\/hot)/,
-      loader: 'babel-loader',
-      query: {
-        presets: [['es2015', {modules: false}]],
-        plugins: []
-      }
+      loader: 'babel-loader'
     })
   }
 })
